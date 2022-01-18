05741a131c452b99430f889d5a8e480454338441
Bob TanStep <boris242008@mail.ru>
2022-01-18 15:36:19 +0300

    finish with icon

--- gulpfile.js
+++ gulpfile.js
@@ -6,28 +6,28 @@ const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
 
 gulp.task('server', function() {
-
     browserSync({
         server: {
-            baseDir: "src"
+            // baseDir: "src"
+            baseDir: "./"
         }
     });
 
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch("./*.html").on('change', browserSync.reload);
 });
 
 gulp.task('styles', function() {
-    return gulp.src("src/sass/**/*.+(scss|sass)")
+    return gulp.src("./sass/**/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({suffix: '.min', prefix: ''}))
         .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("src/css"))
+        .pipe(gulp.dest("./css"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+    gulp.watch("./sass/**/*.+(scss|sass)", gulp.parallel('styles'));
 })
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
